79d7958d951e148c8ee14684ea32b52d290dbb80
KarolusD <kpodzerek@gmail.com>
2023-08-17 21:40:28 +0200

    practice problems of week2

--- week1/hello/hello.c
+++ week1/hello/hello.c
@@ -1,7 +1,8 @@
-#include "/usr/local/include/cs50.h" /* #include <cs50.h> */
+#include <cs50.h>
 #include <stdio.h>
 
 int main(void) {
-  string name = get_string("What's your name? ");
-  printf("hello, %s\n", name);
+    string name = get_string("What's your name? ");
+    printf("hello, %s\n", name);
+    printf("hello, %s\n", name)
 }
